71a6b68adfa8ac5f2c2e75ee225d9ab4850701ba
pranto2051 <prantoislamnt51@gmail.com>
2026-06-11 11:31:27 +0600

    add Coment and some tag

---
+++ C Sharp/Program.cs
@@ -0,0 +1,11 @@
+﻿
+// This Is the First Coment
+Console.WriteLine("Hello, World!");
+
+
+// Use \n and \t use Case
+/*
+ * \n = New Line
+ * \t = Tab that mean Take Space
+ */
+Console.WriteLine("My name is Pranto.\n \t I am a student at IUBAT University at Dahaka, Uttara - 1230");
